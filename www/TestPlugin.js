a847b7dc7c9760167ca6ec33190e7e11e386fa69
Prem Kumar <prem.kumar@zoomrx.com>
2020-11-09 11:50:38 +0530

    Renamed plugin src java

--- www/TestPlugin.js
+++ www/TestPlugin.js
@@ -3,5 +3,5 @@ var cordova = require('cordova');
 window.printNativeLog = function(str, callback) {
     cordova.exec(callback, function(err) {
         callback('Nothing to echo.');
-    }, "Log", "printLog", [str]);
+    }, "AndroidLog", "printLog", [str]);
 };
