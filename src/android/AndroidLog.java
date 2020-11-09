34b4bee2e4609cf3917627f517d84bea4d6449f6
Prem Kumar <prem.kumar@zoomrx.com>
2020-11-09 11:56:39 +0530

    CHanged plugin src code to kotlin

--- src/android/AndroidLog.java
+++ src/android/AndroidLog.java
@@ -1,35 +1,31 @@
-package org.apache.cordova.log;
+package org.apache.cordova.log
 
-import android.util.Log;
-
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.util.Log
+import org.apache.cordova.CallbackContext
+import org.apache.cordova.CordovaPlugin
+import org.json.JSONArray
+import org.json.JSONException
 
 /**
  * This class echoes a string called from JavaScript.
  */
-public class AndroidLog extends CordovaPlugin {
-
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("printLog")) {
-            String message = args.getString(0);
-            this.printLog(message, callbackContext);
-            return true;
+class AndroidLog : CordovaPlugin() {
+    @kotlin.Throws(JSONException::class)
+    override fun execute(action: String, args: JSONArray, callbackContext: CallbackContext): Boolean {
+        if (action == "printLog") {
+            val message = args.getString(0)
+            printLog(message, callbackContext)
+            return true
         }
-        return false;
+        return false
     }
 
-    private void printLog(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            Log.d("printNative", message);
-            callbackContext.success(message);
+    private fun printLog(message: String?, callbackContext: CallbackContext) {
+        if (message != null && message.length > 0) {
+            Log.d("printNative", message)
+            callbackContext.success(message)
         } else {
-            callbackContext.error("Expected one non-empty string argument.");
+            callbackContext.error("Expected one non-empty string argument.")
         }
     }
 }
